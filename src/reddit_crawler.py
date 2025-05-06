3c8921e9164f5e4631cf9dd0d6e0644289f002cf
Nicholas Wong <Nickstorm07@gmail.com>
2025-05-05 21:56:08 -0700

    part A submission

--- src/reddit_crawler.py
+++ src/reddit_crawler.py
@@ -4,11 +4,11 @@ import os
 import time
 import random
 from dotenv import load_dotenv
-from datetime import datetime
 from prawcore.exceptions import TooManyRequests, ServerError, ResponseException
 
 # -------Config-----
-SUBREDDITS = ["Reading"]
+with open("subreddits.txt", 'r') as f:  # Corrected file path
+    SUBREDDITS = [line.strip() for line in f if line.strip()] 
 POSTS_PER_SUBREDDIT = 1000  # Max posts per subreddit
 MAX_COMMENTS_PER_POST = 50  # Max comments per post
 BATCH_SIZE = 10  # Posts per API call
